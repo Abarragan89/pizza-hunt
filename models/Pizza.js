1999740d115f52f71090fe6d827ca57ed80edffd
Anthony Barragan <anthony.bar.89@gmail.com>
2022-04-16 14:49:39 -0700

    added reply subdocument and connected to front end

--- models/Pizza.js
+++ models/Pizza.js
@@ -37,8 +37,8 @@ const PizzaSchema = new Schema({
 
 // get the total count of comments and replies
 PizzaSchema.virtual('commentCount').get(function(){
-    return this.comments.length;
-})
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+});
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
